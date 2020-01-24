6ce75bd476e9abdbd39c950db79af5b9be14e49f
abrodsky <abrodsky@cs.dal.ca>
2020-01-23 21:34:42 -0400

    Initial commit

---
+++ test/SpressoBotTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpressoBotTest {
+
+    @Test
+    void getNextMove() {
+    }
+}
---
+++ test/TimBotTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimBotTest {
+
+    @Test
+    void getID() {
+    }
+
+    @Test
+    void startRound() {
+    }
+
+    @Test
+    void senseDistricts() {
+    }
+
+    @Test
+    void getNextMove() {
+    }
+
+    @Test
+    void isFunctional() {
+    }
+
+    @Test
+    void useShield() {
+    }
+
+    @Test
+    void harvestSpresso() {
+    }
+
+    @Test
+    void fireCannon() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
